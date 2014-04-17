29b0b905a4088cc0a5d346e08eb31e55b740b1fa
gisxy <sxy1985dx@163.com>
2014-04-17 10:04:11 +0800

    iOS7控制中心改造

---
+++ example/ViewController.m
@@ -0,0 +1,69 @@
+//
+//  ViewController.m
+//  RNFrostedSidebar
+//
+//  Created by Ryan Nystrom on 8/13/13.
+//  Copyright (c) 2013 Ryan Nystrom. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (nonatomic, strong) NSMutableIndexSet *optionIndices;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //self.optionIndices = [NSMutableIndexSet indexSetWithIndex:1];
+}
+
+- (IBAction)onBurger:(id)sender {
+    NSArray *images = @[
+                        [UIImage imageNamed:@"gear"],
+                        [UIImage imageNamed:@"globe"],
+                        [UIImage imageNamed:@"profile"],
+                        [UIImage imageNamed:@"star"],
+                        ];
+    NSArray *colors = @[
+                        [UIColor colorWithRed:240/255.f green:159/255.f blue:254/255.f alpha:1],
+                        [UIColor colorWithRed:255/255.f green:137/255.f blue:167/255.f alpha:1],
+//                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+//                        [UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],
+//                        [UIColor colorWithRed:240/255.f green:159/255.f blue:254/255.f alpha:1],
+//                        [UIColor colorWithRed:255/255.f green:137/255.f blue:167/255.f alpha:1],
+                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+                        [UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],];
+//                        [UIColor colorWithRed:240/255.f green:159/255.f blue:254/255.f alpha:1],
+//                        [UIColor colorWithRed:255/255.f green:137/255.f blue:167/255.f alpha:1],
+//                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+//                        [UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],
+//                        ];
+    
+    RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images selectedIndices:self.optionIndices borderColors:colors];
+//    RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images];
+    callout.delegate = self;
+//    callout.showFromRight = YES;
+    [callout show];
+}
+
+#pragma mark - RNFrostedSidebarDelegate
+
+- (void)sidebar:(RNFrostedSidebar *)sidebar didTapItemAtIndex:(NSUInteger)index {
+      
+    self.optionIndices = [NSMutableIndexSet indexSetWithIndex:index];
+}
+
+- (void)sidebar:(RNFrostedSidebar *)sidebar didEnable:(BOOL)itemEnabled itemAtIndex:(NSUInteger)index {
+    
+    if (itemEnabled) {
+        NSLog(@"第二次点击，我要退出啦！");
+    }
+    else{
+        NSLog(@"index %d",index);
+    }
+}
+
+@end
